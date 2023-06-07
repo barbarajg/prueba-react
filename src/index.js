2bec5e3462a5e870218339156c3d254999d70dfa
barbarajg <basjgamboa@gmail.com>
2023-06-07 11:12:38 +0200

    Two hours attempt2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/style.css';
 import App from './App';
-import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
